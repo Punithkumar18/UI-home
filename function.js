b19b1b9e3c2e09065fb2492d18a5c5500057048b
Punithkumar18 <75051555+Punithkumar18@users.noreply.github.com>
2021-03-25 17:04:57 +0530

    initial commit
    First Draft

---
+++ function.js
@@ -0,0 +1,29 @@
+// Get modal
+var modal = document.getElementById("myModal");
+
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the span element thet closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// when the user click on the button, open the modal
+btn.onclick = function(){
+    modal.style.display ="block";
+
+}
+
+// when the user clicks on span (x), close the modal
+span.onclick = function(){
+    
+        modal.style.display ="none";
+
+    }
+
+    // when the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event){
+        if(event.target == modal){
+            modal.style.display = "none";
+        }
+    }
